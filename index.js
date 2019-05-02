c580efb9380afffff525fdf4e58a38a338012e0a
Vicente <you@example.com>
2019-05-02 17:20:07 -0400

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // this entire code, app.get, is a route handler
 app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
+  res.send({ bye: 'buddy'});
 });
 
 //since heroku tells us which port to use, the line below figures out which port it is
