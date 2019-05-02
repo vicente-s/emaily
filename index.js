b2072375eb0d7ec02a9aa187f362420dc95f9c31
Vicente <you@example.com>
2019-05-02 16:58:54 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// this entire code, app.get, is a route handler
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'});
+});
+
+//since heroku tells us which port to use, the line below figures out which port it is
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT);
