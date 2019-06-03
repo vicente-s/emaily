c09c2fc29db11ccdf8722c90f681980b167e1cd5
Vicente <you@example.com>
2019-06-03 11:57:07 -0400

    google OAuth started

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys')
 
-// this entire code, app.get, is a route handler
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log('access token');
+    }
+  )
+);
+
+
+app.get(
+  '/auth/google',
+    passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
 
 //since heroku tells us which port to use, the line below figures out which port it is
 const PORT = process.env.PORT || 5000;
