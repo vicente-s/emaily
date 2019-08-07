faf3867445c5e1c0a9756cf9f1e7e5609ea58d91
Vicente <vicente.soriano@icloud.com>
2019-08-07 15:57:33 -0400

    found require-cookie bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-const cookieSession = require 'cookie-session'
+const cookieSession = require('cookie-session')
 const passport = require('passport')
 require('./models/User');
 require('./services/passport')
