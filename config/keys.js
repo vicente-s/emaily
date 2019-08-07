63dcaa64e520b63cb4a2f7049759d66ee52b4a8e
Vicente <vicente.soriano@icloud.com>
2019-08-07 15:20:47 -0400

    oauth flow finished

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//figure out what set of credentials to return
+if (process.env.NODE_ENV === 'prodcution') {
+  //if we're in production, return production keys
+  module.exports = require('./prod')
+} else {
+  //return the dev keys
+  module.exports = require('./dev');
+}
