7533c6557e9df1f1e2bdd01e2ac5e4e24621c631
Vicente <vicente.soriano@icloud.com>
2019-08-18 16:01:17 -0400

    fixed proxy issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-import App from '/components/App'
-
-
+import App from './components/App'
 
 React.DOM.render(<App />, document.querySelector('#root'));
