cd2de3dc2169c71011f9c725bbd787a9b660846e
Vicente <vicente.soriano@icloud.com>
2019-08-18 16:31:58 -0400

    ReactDOM successfully imported

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
-React.DOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
