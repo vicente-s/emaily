77e6d4e9dc606a48b0bec5bd792347a81e3751ed
Vicente <vicente.soriano@icloud.com>
2019-08-21 10:43:38 -0400

    started navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
 
 import App from './components/App'
 import reducers from './reducers';
